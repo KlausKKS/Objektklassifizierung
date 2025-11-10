c3d767a90142422c7d8118d0f66721ee6a286537
KlausKKS <100866772+KlausKKS@users.noreply.github.com>
2025-11-10 21:46:55 +0100

    Update app_mit_camera_input.py

--- app_mit_camera_input.py
+++ app_mit_camera_input.py
@@ -4,8 +4,9 @@ import cv2
 import numpy as np
 import pandas as pd
 import os
-port = int(os.environ.get("PORT", 10000))
-ui.run(host='0.0.0.0', port=port)
+host = '0.0.0.0'
+port = int(os.environ.get("PORT", 8080))
+ui.run(host=host, port=port)
 import time
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.mobilenet_v3 import preprocess_input
